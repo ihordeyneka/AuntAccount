94e9d06bf2a13f67e4b10a81ec05d14f9e0d3151
Ihor Deyneka <ihordeyneka@gmail.com>
2016-11-06 22:14:16 +0200

    Added Google Maps API

--- Posts/Web/client/scripts/pages/post.js
+++ Posts/Web/client/scripts/pages/post.js
@@ -1,10 +1,45 @@
 define("post", function(){
   var self = this;
+  self.map = null;
+  self.marker = null;
+  self.userPosition = { lat: 40.75773, lng: -73.985708  }; //New York Times Square by default
+
+  self.updateUserPosition = function (position) { //executed when user geolocation data is processed
+    self.userPosition.lat = position.coords.latitude;
+    self.userPosition.lng = position.coords.longitude;
+    if(self.map && self.marker) {
+      $("#inputLocation").val("");
+      updatePosition();
+    }
+  }
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(self.updateUserPosition);
+  }
 
   self.init = function(){
-    console.log("post init");
-    //google api goes here
-    //https://developers.google.com/maps/documentation/javascript/adding-a-google-map
+    require(["https://maps.googleapis.com/maps/api/js?key=AIzaSyANyEK-JVHb9DFlEN1igkGQUD0cT6deZkU&callback=initMap"]);
+  }
+
+  window.initMap = function() {
+    self.map = new google.maps.Map($(".aa-post-map").get(0), {
+      zoom: 10
+    });
+    self.marker = new google.maps.Marker({
+      map: map
+    });
+    updatePosition();
+  }
+
+  self.updatePosition = function(location) {
+    if (!location) {
+      location = userPosition;
+    }
+
+    var latLng = new google.maps.LatLng(location.lat, location.lng);
+
+    self.marker.setPosition(latLng);
+    self.map.panTo(latLng);
   }
 
   return self;
