b6114d17e7caf2e13c7cca3f27eae9e1775d8677
Orysia Deyneka <orysia.deyneka@gmail.com>
2017-01-29 20:01:39 +0200

    cache tokens

--- Posts/API/services/src/main/java/dido.auntaccount.service/business/impl/TokenServiceImpl.java
+++ Posts/API/services/src/main/java/dido.auntaccount.service/business/impl/TokenServiceImpl.java
@@ -1,6 +1,7 @@
 package dido.auntaccount.service.business.impl;
 
 import dido.auntaccount.dao.TokenDAO;
+import dido.auntaccount.dido.auntaccount.utils.CacheMap;
 import dido.auntaccount.entities.Token;
 import dido.auntaccount.service.business.TokenService;
 import org.apache.logging.log4j.Level;
@@ -14,12 +15,17 @@ import java.sql.Date;
 public class TokenServiceImpl implements TokenService {
 
     private static final Logger logger = LogManager.getLogger(TokenServiceImpl.class);
+    private static final CacheMap<String, Long> tokenCacheMap = new CacheMap<>(1000);
 
     @Inject
     TokenDAO tokenDAO;
 
     @Override
     public Token getToken(String token) {
+        if (tokenCacheMap.containsKey(token)) {
+            Long expirationMillis = tokenCacheMap.get(token);
+            return new Token(token, new Date(expirationMillis));
+        }
         return tokenDAO.find(token);
     }
 
@@ -27,10 +33,12 @@ public class TokenServiceImpl implements TokenService {
     public Token saveToken(String token, long expiresIn) {
         DateTime now = DateTime.now();
         DateTime expirationDate = now.plus(expiresIn);
-        Token accessToken = new Token(token, new Date(expirationDate.getMillis()));
+        long expirationDateMillis = expirationDate.getMillis();
+        Token accessToken = new Token(token, new Date(expirationDateMillis));
         Token savedToken = null;
         try {
             savedToken = tokenDAO.save(accessToken);
+            tokenCacheMap.put(token, expirationDateMillis);
         } catch (Exception e) {
             logger.log(Level.ERROR, "Couldn't save token", e);
         }
