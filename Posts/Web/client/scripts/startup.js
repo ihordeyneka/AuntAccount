55dac43aa89109332f0d0d8b394a545b40fde122
Ihor Deyneka <ihordeyneka@gmail.com>
2017-01-29 22:21:20 +0200

    Implemented SignUp page

--- Posts/Web/client/scripts/startup.js
+++ Posts/Web/client/scripts/startup.js
@@ -6,26 +6,3 @@ requirejs.config({
     }
   }
 });
-
-window.globals = {
-  loading: function(element, show) {
-    if (show) {
-      var loadingContainer = $(
-        '<div class="loading-container">\
-        </div>');
-
-      var loading = $(
-          '<div class="loading">\
-            <span class="spinner" />\
-          </div>');
-
-      loading.width(element.width());
-      loading.height(element.height());
-      loadingContainer.append(loading);
-      element.prepend(loadingContainer);
-    }
-    else {
-      element.children(".loading-container").remove();
-    }
-  }
-}
