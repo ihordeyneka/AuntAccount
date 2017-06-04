109bdecbafb087a7c74c4a30471a3d0b544c6a2d
Ihor Deyneka <ihordeyneka@gmail.com>
2017-06-04 17:43:18 +0300

    Removed redundant configuration for alerts

--- Posts/Web/client/scripts/pages/alerts.js
+++ Posts/Web/client/scripts/pages/alerts.js
@@ -16,35 +16,6 @@ define(["../core/globals", "../core/config", "../components/grid", "underscore"]
       var id = $(this).data("uniqueid");
       window.location = "#postdetails/" + id;
     });
-
-    return;
-
-    globals.loading($('body'), true);
-    $.ajax({
-        url: config.apiRoot + "/posts/notifications/"+ userId,
-        dataType: "json"
-    }).done(function(data) {
-      var element = $(".aa-alerts-container");
-      element.empty();
-      if (data.length == 0) {
-        element.append("<h3 class='center'>There were no posts added recently to match your subscriptions.</h3>");
-      } else {
-        for (var i=0; i<data.length; i++) {
-          var post = data[i];
-          element.append($.templates("#templateAlert").render({
-            postId: post.id,
-            time: post.creationDate,
-            title: post.postTags,
-            content: post.description
-          }));
-        }
-      }
-    }).fail(function(result) {
-      self.notificationArea = $(".aa-notification-area").notificationArea();
-      self.notificationArea.error();
-    }).always(function() {
-      globals.loading($('body'), false);
-    });
   }
 
   return self;
