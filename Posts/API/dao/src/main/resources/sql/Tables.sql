8a5949be746c2f88e70eef6fbcdf03c341195e03
Orysia Deyneka <orysia.deyneka@gmail.com>
2017-09-24 23:48:44 +0300

    utf-8 encoding

--- Posts/API/dao/src/main/resources/sql/Tables.sql
+++ Posts/API/dao/src/main/resources/sql/Tables.sql
@@ -3,7 +3,7 @@ create database if not exists dido  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE u
 CREATE TABLE `dido`.`Post` (
   `Id` bigint(20) NOT NULL,
   `UserId` bigint(10) NOT NULL,
-  `Description` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) CHARACTER SET 'utf8' DEFAULT NULL,
   `LocationId` bigint(10) NOT NULL,
   `PriceMin` float DEFAULT NULL,
   `PriceMax` float DEFAULT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `dido`.`Post` (
 
 CREATE TABLE `dido`.`Tag` (
   `Id` bigint(10) NOT NULL,
-  `Tag` varchar(20) NOT NULL,
+  `Tag` varchar(20) CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (`Id`)
 );
 
@@ -26,9 +26,9 @@ CREATE TABLE `dido`.`PostTag` (
 
 CREATE TABLE `USER` (
   `Id` bigint(10) NOT NULL AUTO_INCREMENT,
-  `FirstName` varchar(45) NOT NULL,
-  `LastName` varchar(45) DEFAULT NULL,
-  `Password` varchar(200),
+  `FirstName` varchar(45) CHARACTER SET 'utf8' NOT NULL,
+  `LastName` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
+  `Password` varchar(200) CHARACTER SET 'utf8',
   `LocationId` bigint(10) DEFAULT NULL,
   `Email` varchar(45) NOT NULL,
   `Phone` varchar(45) DEFAULT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE `USER` (
 
 CREATE TABLE `dido`.`SellerReview` (
   `Id` BIGINT(10) NOT NULL,
-  `Description` VARCHAR(45) NULL,
+  `Description` VARCHAR(45) CHARACTER SET 'utf8' NULL,
   `Rate` INT NULL,
   `UserId` BIGINT(10) NOT NULL,
   `SellerId` BIGINT(10) NOT NULL,
@@ -56,16 +56,16 @@ CREATE TABLE `LOCATION` (
   `Id` bigint(10) NOT NULL AUTO_INCREMENT,
   `Latitude` varchar(45) NOT NULL,
   `Longitude` varchar(45) NOT NULL,
-  `Country` varchar(45) DEFAULT NULL,
-  `City` varchar(45) DEFAULT NULL,
-  `Place` varchar(45) NOT NULL,
-  `PlaceId` varchar(45) NOT NULL,
-  `Region1` varchar(45) DEFAULT NULL,
-  `Region2` varchar(45) DEFAULT NULL,
-  `Name` varchar(45) DEFAULT NULL,
+  `Country` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
+  `City` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
+  `Place` varchar(45) CHARACTER SET 'utf8' NOT NULL,
+  `PlaceId` varchar(45) CHARACTER SET 'utf8' NOT NULL,
+  `Region1` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
+  `Region2` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
+  `Name` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
   `StreetNumber` int(11) DEFAULT NULL,
-  `Neighborhood` varchar(45) DEFAULT NULL,
-  `ROUTE` varchar(45) DEFAULT NULL,
+  `Neighborhood` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
+  `ROUTE` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
   `Radius` double DEFAULT NULL,
   PRIMARY KEY (`Id`)
 );
@@ -73,14 +73,14 @@ CREATE TABLE `LOCATION` (
 
 
 CREATE TABLE `dido`.`Country` (
-  `Country` VARCHAR(45) NOT NULL,
+  `Country` VARCHAR(45) CHARACTER SET 'utf8' NOT NULL,
   PRIMARY KEY (`Country`)
 );
 
 
 CREATE TABLE `dido`.`SellerTag` (
      `SellerId` BIGINT(10) NOT NULL,
-     `TagId`      BIGINT(10) NOT NULL,
+     `TagId` BIGINT(10) NOT NULL,
      PRIMARY KEY (`SellerId`, `TagId`)
 );
 
@@ -101,7 +101,7 @@ CREATE TABLE `dido`.`Message` (
   `Id` bigint(10) NOT NULL,
   `SenderId` bigint(10) NOT NULL,
   `OfferId` bigint(10) NOT NULL,
-  `Description` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) CHARACTER SET 'utf8' DEFAULT NULL,
   `Photo` longblob DEFAULT NULL,
   `CreationDate` datetime NOT NULL,
   `IsRead` tinyint(1) NOT NULL,
@@ -125,7 +125,7 @@ CREATE TABLE `SELLER` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Rate` float DEFAULT NULL,
   `UserId` int(11) NOT NULL,
-  `Name` varchar(45) DEFAULT NULL,
+  `Name` varchar(45) CHARACTER SET 'utf8' DEFAULT NULL,
   `Website` varchar(100) DEFAULT NULL,
   `Phone` varchar(45) DEFAULT NULL,
   `CreationDate` datetime DEFAULT NULL,
